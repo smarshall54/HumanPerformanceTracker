6cd816dfff6433d3eea6a8f310f43d91e3c45e35
smarshall54 <smarshall54@gmail.com>
2015-08-19 07:05:36 -0400

    data to be logged

---
+++ setup_logdata.m
@@ -0,0 +1,60 @@
+function data = setup_logdata
+
+    %% weight
+        % daily AM
+        % pre training
+        % post training
+        
+    
+    
+    
+    %% nutrition
+        % calories consumed
+        % protein
+        % fat
+        % carbs
+        % sugar
+        % sodium
+        % fiber
+        % water intake
+    
+    
+    
+    
+    %% biomarkers
+    `   % MAX HR
+        % VO2 MAX
+        % Resting HR
+        % Lactate Threshold
+        
+        % HDL/LDL/TRIG
+        % Hormones
+        %   e.g. test/est
+        % blood glucose
+        % blood ketones
+        % blood pressure
+    
+    
+    
+    %% resistance exercise
+        % Lift
+        % Sets
+        % Reps
+        % Weight
+        % Rest
+    
+    
+    
+    %% cardiovascular exercise
+        % Parameters:
+        %    # of "sets"
+        %    duration
+        %    intensity
+        %    rest
+        %    heart rate
+        %    type
+        %       e.g. running, biking, rowing, swimming, stairclimber
+    
+    
+    
+end
